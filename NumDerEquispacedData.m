958f9b8b5b0823ea95de142d013d8f252ee1220d
MichaelYoussef9 <131902068+MichaelYoussef9@users.noreply.github.com>
2023-04-27 18:19:22 +0200

    Update NumDerEquispacedData.m

--- NumDerEquispacedData.m
+++ NumDerEquispacedData.m
@@ -1,20 +1,23 @@
 function [d,ifail] = NumDerEquispacedData(a,b,nu,f)
-% [d,ifail] = NumDerEquispacedData(a,b,nu,f)
+% NumDerEquispacedData computes the derivatives of a function F starting from its values at uniformly distributed points
+%
 % INPUT:
-% [a,b]    - domanin of the function F
+% [a,b]    - domain of the function F
 % nu       - order of the computed derivative of F, o=1,2,...
 % f(1:n+1) - vector containing the values of F at n+1 uniformly distributed points
 %            in [a,b] ---> f(j)=F(a+(j-1)(b-a)/n);
+%
 % OUTPUT:
 % d(1:m)   - derivative of order nu of F, m=n-nu+1, m must be >=3; 
 %            d(j)=derivative of order nu of F at x=a+(j-1+nu/2)(b-a)/n;
 % ifail    - Error Indicators and Warnings 
 %            ifail=0 --> no error detected;
 %            ifail=1 --> on entry  a>=b or o<1;
-%            ifail=2 --> the method cannot be applied because n<o+2,
+%            ifail=2 --> the method cannot be applied because n<o+2.
 
-n=length(f);
-n=n-1;
+n=length(f)-1;
+
+% Check for error indicators and warnings
 ifail=0;
 if (a>=b || nu<1)
     ifail=1;
@@ -25,8 +28,10 @@ if ifail >0
     d=0*f;
     return;
 end
-hTrue=(b-a)/n; % discretiztion step in [0,1]
-h=1/n; % discretiztion step in [0,1]
+
+% Discretization step in [a,b]
+hTrue=(b-a)/n; 
+
 g=f(2:n+1)-f(1); 
 gg=2*g; 
 gg(n)=g(n);
@@ -43,12 +48,17 @@ gtp0=(-1689*f(1)+1005*f(2)+1430*f(3)-1110*f(4)+435*f(5)-71*f(6))/(1920*h);
 gtpnm1=-(-1689*f(n+1)+1005*f(n)+1430*f(n-1)-1110*f(n-2)+435*f(n-3)-71*f(n-4))/(1920*h);
 AppcL0=(sqrt(2*n)/24)*(-26*g(1)+g(2))+(sqrt(2/n))*gtp0;
 AppcLnm1=(sqrt(2*n)/24)*(-27*g(n)-g(n-2)+28*g(n-1))+(sqrt(2/n))*gtpnm1;
+
 for s=0:n-1
   App=AppcL0*cL0(s+1)+AppcLnm1*cLnm1(s+1);
   Fg(s+1)=(n*sqrt(2)/24)*(27*sL0(s+1)-sL1(s+1))*dst(s+1)+App;
 end
+
+% Computation of the first order derivative
 d=dct(Fg,'Type',4)/(b-a);
-for p=2:nu %computation of derivatives of order p> 1, p<=nu.
+
+% Computation of derivatives of order p> 1, p<=nu
+for p=2:nu 
     m=n-p+1;
     na=a+hTrue*(p-1)/2;
     nb=a+hTrue*(m+(p-1)/2);
